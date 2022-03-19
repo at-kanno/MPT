f76aa9762b875ed6e0a0e8e65415ceb89de601fd
at-kanno <kanno@olivenet.co.jp>
2022-03-19 15:03:51 +0900

    背景色を４つにした

--- constant.py
+++ constant.py
@@ -1,7 +1,13 @@
-#LOGIN_URL = 'http://54.163.203.19:7000/'
-#SECOND_TEST = "修了試験"
-#MODE = 1
+#
+#    研修用サービス
+#
+LOGIN_URL = 'http://54.163.203.19:7000/'
+SECOND_TEST = "修了試験"
+MODE = 1
 
-LOGIN_URL = 'http://54.163.203.19:5000/'
-SECOND_TEST = "実力確認試験"
-MODE = 2
+#
+#    模擬試験サービス
+#
+#LOGIN_URL = 'http://54.163.203.19:5000/'
+#SECOND_TEST = "実力確認試験"
+#MODE = 2
